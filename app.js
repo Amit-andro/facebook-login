cde54f6a312fdf677ab8ed4976d0df202ac0eb98
Amit jaiswal <65877511+Amit-andro@users.noreply.github.com>
2024-07-14 00:28:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 window.fbAsyncInit = function() {
     FB.init({
-      appId            : '795422859405830',
+      appId            : '471832532401636',
       autoLogAppEvents : true,
       xfbml            : true,
       version          : 'v20.0'
     });
   
-    // Check if the user is already logged in
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
   };
   
-  // Load the SDK asynchronously
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
@@ -21,12 +19,10 @@ window.fbAsyncInit = function() {
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
   
-  // Function to handle the response from Facebook login
   function statusChangeCallback(response) {
     if (response.status === 'connected') {
       console.log('Logged in and authenticated');
       document.getElementById('status').innerHTML = 'Logged in and authenticated';
-      // Fetch user data or perform actions on behalf of the user
       fetchUserData();
     } else {
       console.log('Not authenticated');
@@ -34,14 +30,12 @@ window.fbAsyncInit = function() {
     }
   }
   
-  // Function to handle login
   function login() {
     FB.login(function(response) {
       statusChangeCallback(response);
     }, {scope: 'public_profile,email'});
   }
   
-  // Function to fetch user data
   function fetchUserData() {
     FB.api('/me', {fields: 'name,email'}, function(response) {
       console.log('Successful login for: ' + response.name);
